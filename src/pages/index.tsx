5fd24a5ac6df24342210703b1621257618ee99cd
Isaac Turner <spacey_sooty@outlook.com>
2024-01-03 21:44:58 +0800

    add who are we

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -20,8 +20,8 @@ export default function Home() {
           </div>
         </div>
         <div className='heros'>
-          <h2>More Content</h2>
-          <p>content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content </p>
+          <h2>Who are we?</h2>
+          <p>We are a community First Robotics Competition team based out of Curtin University from Western Australia (WA). Founded in 2013 we were the very first First team in WA and have started a variety of First programs here in WA. Along with ths we have also competed very succesfully in FRC for many years. Most Recently in 2023 we began competing at regionals again after COVID and won the Impact award sending us to Champs.</p>
           <h2>More Content</h2>
           <h2>More Content</h2>
           <h2>More Content</h2>
